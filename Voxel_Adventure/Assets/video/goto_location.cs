cd1ce22b0933d020d36add134b90547fbd4eb4c5
jonathanritson42 <jonathanritson42@gmail.com>
2019-03-14 17:46:30 +0000

    Second level updated

---
+++ Voxel_Adventure/Assets/video/goto_location.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goto_location : MonoBehaviour
+{
+    public Camera MainCamera;
+    public Transform TargetPosition;
+    private float speed = 0.1f;
+    public Transform target;
+
+
+    private void Update()
+    {
+        transform.LookAt(target);
+
+        MainCamera.transform.position = Vector3.Lerp(transform.position, TargetPosition.transform.position, speed * Time.deltaTime);
+        MainCamera.transform.rotation = Quaternion.Lerp(transform.rotation, TargetPosition.transform.rotation, speed * Time.deltaTime);
+    }
+}
---
+++ Voxel_Adventure/Assets/video/jump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jump : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine(jumping());
+    }
+
+    IEnumerator jumping()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(1.0f, 5.0f));
+
+            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
+        }
+    }
+}
