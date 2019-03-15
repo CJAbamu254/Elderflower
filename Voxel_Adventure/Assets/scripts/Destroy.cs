cb9b45548b8d154ad4687aaed75da71d80eb32a7
jonathanritson42 <jonathanritson42@gmail.com>
2019-03-15 01:54:36 +0000

    Forest and Mall updated

---
+++ Voxel_Adventure/Assets/scripts/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject DestroyPS;
+
+
+    #region NOTICE
+    // OnMouseDown is treated as a Raqycast Function but without any effort. Just apply this script to th object you want to destroy
+    // Apply a Collider to the object you wish to destroy and apply this script to that obect
+    // he object will die when you click the left mouse button
+    #endregion
+    private void OnMouseDown()
+    {
+        Instantiate(DestroyPS, transform.position, DestroyPS.transform.rotation);
+        Destroy(gameObject);
+    }
+}
+
---
+++ Voxel_Adventure/Assets/scripts/DestroyComplete.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyComplete : MonoBehaviour
+{
+
+    private void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+}
