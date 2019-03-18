44e3ad2aae7ffb0bcb0b76aed8bc3c08a9dde6d7
jonathanritson42 <jonathanritson42@gmail.com>
2019-03-18 01:54:01 +0000

    UI intergration for bedroom

--- Voxel_Adventure/Assets/scripts/Home.cs
+++ Voxel_Adventure/Assets/scripts/Home.cs
@@ -10,11 +10,21 @@ public class Home : MonoBehaviour
     public Button Homebut, Listbut;
 
     public Image levellist;
+    public float moveDistance;
+    public Vector3 Startpos;
+    public Vector3 Targetpos;
+    private float move;
+    private float CurrentLerpTime;
+    private float LerpTime = 0.5f;
+    private float perc;
+
 
     private void Start()
     {
         Homebut.onClick.AddListener(TaskOnClick);
         Listbut.onClick.AddListener(TaskOnClick1);
+
+        //startpos = levellist.transform.position;
     }
 
     void TaskOnClick()
@@ -24,10 +34,53 @@ public class Home : MonoBehaviour
 
     void TaskOnClick1()
     {
-        // transform list image up from below.
+        if (move == 0)
+        {
+            Startpos = levellist.GetComponent<Transform>().position;
+            Targetpos = Startpos + Vector3.up * moveDistance;
+            move = 1;
+        }
 
-        //MainCamera.transform.position = Vector3.Lerp(transform.position, TargetPosition.transform.position, speed * Time.deltaTime);
-        //MainCamera.transform.rotation = Quaternion.Lerp(transform.rotation, TargetPosition.transform.rotation, speed * Time.deltaTime);
+        if (move == 2)
+        {
+            Startpos = levellist.GetComponent<Transform>().position;
+            Targetpos = Startpos + Vector3.down * moveDistance;
+            move = 3;
+        }
+    }
 
+    private void Update()
+    {
+        if (move == 1)
+        {
+            CurrentLerpTime += Time.deltaTime;
+            if (CurrentLerpTime > LerpTime)
+            {
+                CurrentLerpTime = LerpTime;
+            }
+            perc = CurrentLerpTime / LerpTime;
+            levellist.transform.position = Vector3.Lerp(Startpos, Targetpos, perc);
+            if (perc == 1)
+            {
+                move = 2;
+                CurrentLerpTime = 0f;
+            }
+        }
+
+        if (move == 3)
+        {
+            CurrentLerpTime += Time.deltaTime;
+            if (CurrentLerpTime > LerpTime)
+            {
+                CurrentLerpTime = LerpTime;
+            }
+            perc = CurrentLerpTime / LerpTime;
+            levellist.transform.position = Vector3.Lerp(Startpos, Targetpos, perc);
+            if (perc == 1)
+            {
+                move = 0;
+                CurrentLerpTime = 0f;
+            }
+        }
     }
 }
