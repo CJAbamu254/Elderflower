6e6c7dc743e68434bf121babff5c81ce1a7c2864
jonathanritson42 <jonathanritson42@gmail.com>
2020-04-18 07:23:03 +0100

    AR warning added

--- Voxel_Adventure/Assets/scripts/levelload.cs
+++ Voxel_Adventure/Assets/scripts/levelload.cs
@@ -9,6 +9,11 @@ public class levelload : MonoBehaviour
 
     public Button Bed, For, Mal, Zoo, Mus, Tra;
 
+    public GameObject loading;
+    private bool load;
+
+    private int levelnum;
+
     // Start is called before the first frame update
     void Update()
     {
@@ -18,40 +23,84 @@ public class levelload : MonoBehaviour
         Zoo.onClick.AddListener(TaskOnClick3);
         Mus.onClick.AddListener(TaskOnClick4);
         Tra.onClick.AddListener(TaskOnClick5);
+
+        //if (load)
+        //{
+        //    loading.GetComponent<Text>().color = new Color(loading.GetComponent<Text>().color.r, loading.GetComponent<Text>().color.g, loading.GetComponent<Text>().color.b, Mathf.PingPong(Time.time, 1));
+        //}
     }
 
     void TaskOnClick()
     {
-        SceneManager.LoadScene(1, LoadSceneMode.Single);
+        StartCoroutine(GameObject.FindObjectOfType<fadeforui>().FadeAndLoadScene(fadeforui.FadeDirection.In));
+
+        levelnum = 1;
+
+        StartCoroutine(LoadNewScene());
     }
 
     void TaskOnClick1()
     {
-        SceneManager.LoadScene(2, LoadSceneMode.Single);
+        StartCoroutine(GameObject.FindObjectOfType<fadeforui>().FadeAndLoadScene(fadeforui.FadeDirection.In));
 
+        levelnum = 2;
+
+        StartCoroutine(LoadNewScene());
     }
 
     void TaskOnClick2()
     {
-        SceneManager.LoadScene(3, LoadSceneMode.Single);
+        StartCoroutine(GameObject.FindObjectOfType<fadeforui>().FadeAndLoadScene(fadeforui.FadeDirection.In));
 
+        levelnum = 3;
+
+        StartCoroutine(LoadNewScene());
     }
 
     void TaskOnClick3()
     {
-        SceneManager.LoadScene(4, LoadSceneMode.Single);
+        StartCoroutine(GameObject.FindObjectOfType<fadeforui>().FadeAndLoadScene(fadeforui.FadeDirection.In));
 
+        levelnum = 4;
+
+        StartCoroutine(LoadNewScene());
     }
 
     void TaskOnClick4()
     {
-        SceneManager.LoadScene(5, LoadSceneMode.Single);
+        StartCoroutine(GameObject.FindObjectOfType<fadeforui>().FadeAndLoadScene(fadeforui.FadeDirection.In));
 
+        levelnum = 5;
+
+        StartCoroutine(LoadNewScene());
     }
 
     void TaskOnClick5()
     {
-        SceneManager.LoadScene(6, LoadSceneMode.Single);
+        StartCoroutine(GameObject.FindObjectOfType<fadeforui>().FadeAndLoadScene(fadeforui.FadeDirection.In));
 
+        levelnum = 6;
+
+        StartCoroutine(LoadNewScene());
+    }
+
+    IEnumerator LoadNewScene()
+    {
+        yield return new WaitForSeconds(3);
+
+        loading.SetActive(true);
+        
+        //load = true;
+
+        yield return new WaitForSeconds(1);
+
+        SceneManager.LoadScene(levelnum);
+
+        //AsyncOperation async = SceneManager.LoadSceneAsync(levelnum);
+
+        //while (!async.isDone)
+        //{
+        //    yield return null;
+        //}
     }
 }
---
+++ Voxel_Adventure/Assets/scripts/warningfade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class warningfade : MonoBehaviour
+{
+    public Image warning, parent;
+    private float alpha = 1;
+
+    void Update()
+    {
+        if (warning.color.a <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        else
+        {
+            Color newColor = new Color(1, 1, 1, alpha);
+            warning.color = newColor;
+            parent.color = newColor;
+        }
+
+        alpha -= 0.004f;
+    }
+}
